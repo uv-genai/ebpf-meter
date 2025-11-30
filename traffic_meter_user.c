39545dcb63a28125451f7b66c0c30cd409cf8d3d
ugo <ugovaretto@gmail.com>
2025-11-30 16:52:33 +0800

    CSV

--- traffic_meter_user.c
+++ traffic_meter_user.c
@@ -134,7 +134,7 @@ int main(int argc, char **argv)
                 snprintf(filename, sizeof(filename), "/tmp/traffic_user_%u.log", next_uid);
                 FILE *f = fopen(filename, "a");
                 if (f) {
-                    fprintf(f, "bytes=%llu src=%s dst=%s\n", (unsigned long long)val.bytes, src, dst);
+                    fprintf(f, "%llu,%s,%s\n", (unsigned long long)val.bytes, src, dst);
                     fclose(f);
                 }
             }
